fdbc2cfd96e5ab664fcace6cca71851d4f79ffea
James Rhodes <jrhodes@redpointsoftware.com.au>
2014-02-18 08:24:52 +1100

    add notes on ship layout requirements

--- Mir/ShipLayout.cs
+++ Mir/ShipLayout.cs
@@ -4,6 +4,17 @@ namespace Mir
 {
     public class ShipLayout
     {
+        /*
+         * TODO
+         * 
+         * Need a 3D grid based layout with support for arbitrary wiring along the cell walls.  Normal
+         * corridors are made of 2xNx2 cells; cells can be empty, or contain a blockable object (such
+         * as a DCPU terminal).
+         * 
+         * We need to support rooms of arbitrary sizes (in all 3 dimensions).
+         * 
+         */
+
         public ShipLayout()
         {
         }
